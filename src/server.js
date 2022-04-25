123bbb95dbffdf847ef1039b4d894763877745e0
LucasLyra01 <lucaslyra8@gmail.com>
2022-04-24 21:40:05 -0300

    API - v2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config({
-    path: '.env'
+    path: 'env'
 });
 
 const express = require('express');
@@ -16,12 +16,33 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        status: 'ok',
-        message: 'Servidor rodando'
-    });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.methods === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, ´POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
 });
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado')
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.json({
+        erro: error.message
+    })
+})
 
 app.listen(port, hostname, () => {
     console.log(`Servidor rodando no endereço ${hostname}:${port}`)
